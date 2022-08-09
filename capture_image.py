069d03a391f42cdc4ebc22551e1cc4128f7aa2a4
Vishal Sharma <33948196+vishal0021@users.noreply.github.com>
2022-08-09 16:06:17 +0530

    Update capture_image.py

--- capture_image.py
+++ capture_image.py
@@ -2,6 +2,8 @@ lat = "36.9848° S"
 long = "143.3906° E"
 
 size = 200 #km
-bb = getBoundingBox(Lat, Long, size)
-image = getImageAt(Bounding box, camera, repeat_state)
+b_box = getBoundingBox(Lat, Long, size)
+#camera 1 is Nadir
+#no repeat 
+image = getImageAt(b_box, 1, False)
 display(image)
